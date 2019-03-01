4d06980b7eaf38768df7a9b757ae8d9fa47c8c4a
nitultalukdar <48099631+nitultalukdar@users.noreply.github.com>
2019-03-01 16:40:58 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Nitul!!  U are Awesome !!!";
+    private final String message = "Hello World!";
 
     public App() {}
 
